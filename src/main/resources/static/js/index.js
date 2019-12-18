d07b80174f5b1adf2bd8ad9690f4fe0d8e543290
s-115-18 <s-115-18@gumiforest.ae.krakow.pl>
2019-12-18 09:24:02 +0100

    JS

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,18 @@
 const getCards = () = {
     const cardsIndexURL ="/api/cards/balances"
     const response = await fetch(cardsIndexURL)
-    return await response.json();
+    const data = await response.json()
+    return data;
 }
-console.log(getCards())
+const handleAppendCards = (cards) => {
+    const cardsListE1 = document.querySelector('.cards__list')
+    cards
+        .map(card => `<li>${card.number} | ${card.saldo}</li>`)
+        .forEach(cardHtml => cardsListE1.insertAdjacentHTML("beforeend", cardHtml))
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cardsListE1 = document.querySelector('.cards__list')
+    getCards()
+        .then(cards => handleAppendCards(cards, cardsListE1));
+})
