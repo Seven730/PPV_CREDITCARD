52a39c5a958afa32b0a0f52b1e7daf609eb0fb03
s-115-18 <s-115-18@gumiforest.ae.krakow.pl>
2019-12-18 09:00:54 +0100

    frontend things

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,6 @@
+const getCards = () = {
+    const cardsIndexURL ="/api/cards/balances"
+    const response = await fetch(cardsIndexURL)
+    return await response.json();
+}
+console.log(getCards())
